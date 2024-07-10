c69ba4b1602ac169f01c4b385add662d7a85050f
jaymejunior25 <jaymejunior@yahoo.com.br>
2024-07-10 13:01:45 -0300

    testes 2 lab master

--- processarRlabmaster.php
+++ processarRlabmaster.php
@@ -10,6 +10,7 @@ if (!isset($_SESSION['user_id'])) {
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $pacotes = json_decode($_POST['pacotes'], true);
     $laboratorio_id = $_POST['laboratorio_id'];
+    $laboratorioM_id = 20;
 
     foreach ($pacotes as $pacote) {
         $codigobarras = $pacote['codigobarras'];
@@ -18,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $digitoverificarp = substr($codigobarras, 0, 1);
         $digitoverificaru = substr($codigobarras, -1);
 
-        if (($digitoverificarp == 'A' || $digitoverificarp == 'a') && ($digitoverificaru == 'B' || $digitoverificaru == 'b')) {
+        if (($digitoverificarp === 'A' || $digitoverificarp === 'a') && ($digitoverificaru === 'B' || $digitoverificaru === 'b')) {
             $codigobarras = substr($codigobarras, 1, -1);
         } else {
             echo json_encode(['status' => 'error', 'message' => 'Código de barras inválido para LABMASTER.']);
@@ -26,16 +27,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
 
         // Verificar se o pacote tem status "enviado"
-        $stmt = $dbconn->prepare("SELECT status FROM pacotes WHERE codigobarras = :codigobarras AND lab_id = :laboratorio_id");
-        $stmt->execute([':codigobarras' => $codigobarras, ':laboratorio_id' => $laboratorio_id]);
+        $stmt = $dbconn->prepare("SELECT * FROM pacotes WHERE codigobarras = :codigobarras AND lab_id = :laboratorio_id");
+        $stmt->execute([':codigobarras' => $codigobarras, ':laboratorio_id' => $laboratorioM_id]);
         $pacote = $stmt->fetch(PDO::FETCH_ASSOC);
 
         if ($pacote && $pacote['status'] === 'enviado') {
-            $stmt = $dbconn->prepare("UPDATE pacotes SET data_recebimento = NOW(), status = 'recebido', usuario_recebimento_id = :usuario_recebimento_id WHERE codigobarras = :codigobarras AND lab_id = :laboratorio_id");
+            $stmt = $dbconn->prepare("UPDATE pacotes SET data_recebimento = NOW(), status = 'recebido', lab_id = :laboratorio_id , usuario_recebimento_id = :usuario_recebimento_id WHERE codigobarras = :codigobarras");
             $stmt->execute([
+                ':laboratorio_id' => $laboratorio_id,
                 ':usuario_recebimento_id' => $_SESSION['user_id'],
-                ':codigobarras' => $codigobarras,
-                ':laboratorio_id' => $laboratorio_id
+                ':codigobarras' => $codigobarras
             ]);
         }
     }
--- processar_pacotesR.php
+++ processar_pacotesR.php
@@ -12,32 +12,39 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     foreach ($pacotes as $pacote) {
         $codigobarras = $pacote['codigobarras'];
+        $codigobarrasFiltrado = $pacote['codigobarrasFiltrado'];
 
         // Verificação e processamento do código de barras
         $digitoverificarp = substr($codigobarras, 0, 1);
         $digitoverificaru = substr($codigobarras, -1);
-        if ($digitoverificarp == '=' && ctype_digit($digitoverificaru)) {
+
+        
+        if ($digitoverificarp === '=' && ctype_digit($digitoverificaru)) {
             $codigobarras = substr($codigobarras, 1);
-        } elseif ($digitoverificarp == 'B' || $digitoverificarp == 'b' && ctype_digit($digitoverificaru)) {
+
+        } elseif (($digitoverificarp === 'B' || $digitoverificarp === 'b') && ctype_digit($digitoverificaru)) {
             $codigobarras = substr_replace($codigobarras, '0', -2, 1);
+
         } else {
             $codigobarras = substr($codigobarras, 1, -1);
+
         }
 
         // Verificar se o pacote tem status "enviado"
-        $stmt = $dbconn->prepare("SELECT status FROM pacotes WHERE codigobarras = :codigobarras");
-        $stmt->execute([':codigobarras' => $codigobarras]);
+        $stmt = $dbconn->prepare("SELECT * FROM pacotes WHERE codigobarras = :codigobarras");
+        $stmt->execute([':codigobarras' => $codigobarrasFiltrado]);
         $pacote = $stmt->fetch(PDO::FETCH_ASSOC);
 
         if ($pacote && $pacote['status'] === 'enviado') {
             $stmt = $dbconn->prepare("UPDATE pacotes SET data_recebimento = NOW(), status = 'recebido', usuario_recebimento_id = :usuario_recebimento_id WHERE codigobarras = :codigobarras");
             $stmt->execute([
                 ':usuario_recebimento_id' => $_SESSION['user_id'],
-                ':codigobarras' => $codigobarras
+                ':codigobarras' => $codigobarrasFiltrado
             ]);
+           
         }
-    }
 
+    }
     echo json_encode(['status' => 'success', 'message' => 'Pacotes recebidos com sucesso!']);
 }
 ?>

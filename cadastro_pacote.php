aa36eb660ac6f945c36539a33069ef729c02cc7e
jaymejunior25 <jaymejunior@yahoo.com.br>
2024-07-15 10:28:54 -0300

    prefixos

--- cadastro_pacote.php
+++ cadastro_pacote.php
@@ -58,8 +58,11 @@
                 return codigoBarras.slice(0, -2) + '0' + codigoBarras.slice(-1);
             } else if ((digitoverificarp === 'A' || digitoverificarp === 'a') && (digitoverificaru === 'B' || digitoverificaru === 'b')) {
                 return codigoBarras.slice(1, -1);
-            } else {
+            } else if ((digitoverificarp === 'A' || digitoverificarp === 'A') && (digitoverificaru === 'A' || digitoverificaru === 'a')){
                 return codigoBarras.slice(1, -1);
+            }
+            else{
+                return codigoBarras;
             }
         }
 
